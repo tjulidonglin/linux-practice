98f51e820c50b0c45a1cd3021793f1d5cc0f7711
tjulidonglin <tjulidonglin@live.cn>
2019-05-11 15:29:28 +0800

    Create clean-ubuntu.sh
    After installation of ubuntu os, we can run this script for removing some extra softwares which we don't use mostly.

---
+++ clean-ubuntu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt-get remove libreoffice-common
+apt-get remove unity-webapps-common
+apt-get remove thunderbird totem rhythmbox empathy brasero simple-scan gnome-mahjongg aisleriot gnome-mines cheese transmission-common gnome-orca webbrowser-app gnome-sudoku  landscape-client-ui-install
+apt-get remove onboard deja-dup
