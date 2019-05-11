3ea65360eb010c7cecece3f04bb95cd4fd0754e6
tjulidonglin <tjulidonglin@live.cn>
2019-05-11 15:36:10 +0800

    Create uninstall-aliyun.sh
    购买阿里云服务器后，执行此脚本干掉虚机内部阿里云顿

---
+++ scripts/uninstall-aliyun.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Step1: 卸载阿里云盾
+wget http://update.aegis.aliyun.com/download/uninstall.sh
+chmod +x uninstall.sh
+./uninstall.sh
+wget http://update.aegis.aliyun.com/download/quartz_uninstall.sh
+chmod +x quartz_uninstall.sh
+./quartz_uninstall.sh
+
+#Step2: 删除残留
+pkill aliyun-service
+rm -fr /etc/init.d/agentwatch /usr/sbin/aliyun-service
+rm -rf /usr/local/aegis*
+
+#Step3: 屏蔽阿里云盾IP
+iptables -I INPUT -s 140.205.201.0/28 -j DROP
+iptables -I INPUT -s 140.205.201.16/29 -j DROP
+iptables -I INPUT -s 140.205.201.32/28 -j DROP
+iptables -I INPUT -s 140.205.225.192/29 -j DROP
+iptables -I INPUT -s 140.205.225.200/30 -j DROP
+iptables -I INPUT -s 140.205.225.184/29 -j DROP
+iptables -I INPUT -s 140.205.225.183/32 -j DROP
+iptables -I INPUT -s 140.205.225.206/32 -j DROP
+iptables -I INPUT -s 140.205.225.205/32 -j DROP
+iptables -I INPUT -s 140.205.225.195/32 -j DROP
+iptables -I INPUT -s 140.205.225.204/32 -j DROP
