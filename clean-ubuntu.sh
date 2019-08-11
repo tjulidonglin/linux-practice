090fc26f0247ef57369cd0a326b023b46018f9a6
tjulidonglin <tjulidonglin@live.cn>
2019-08-11 15:54:58 +0800

    clean-ubuntu.sh: add some softwares to be removed
    remove some extra softwares

--- clean-ubuntu.sh
+++ clean-ubuntu.sh
@@ -3,3 +3,5 @@ apt-get remove libreoffice-common
 apt-get remove unity-webapps-common
 apt-get remove thunderbird totem rhythmbox empathy brasero simple-scan gnome-mahjongg aisleriot gnome-mines cheese transmission-common gnome-orca webbrowser-app gnome-sudoku  landscape-client-ui-install
 apt-get remove onboard deja-dup
+apt-get remove gnome-software gnome-todo gedit gnome-power-manager update-manager update-manager-common update-nitifier update-notifier-common 
+apt-get autoremove
