0e26f228222da507e334dfb030e32dee107099bf
lidonglin <tjulidonglin@live.cn>
2019-08-25 02:08:35 -0400

    install build env for centos
    add script for installing dependence rpms

---
+++ ci-scripts/centos-install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+yum install -y gcc git wget vim cscope ctags
+#yum install -y openssh-server gcc g++ make autopoint pkgconf zlib1g-dev libsdl2-dev libglib2.0-dev libpixman-1-dev texinfo libusb-1.0-0-dev libspice-server-dev libncurses5-dev libtool autoconf libxml2-utils xsltproc libxml2-dev libpciaccess-dev libyajl-dev tree python3-pip libnl-3-dev libnl-route-3-dev libdevmapper-dev openvswitch-switch net-tools git vim samba openssh-server pkg-config gnutls-bin openvswitch-switch 
+yum install -y zlib-devel SDL-devel glib2-devel pixman-devel spice-server spice-server-devel libcap-devel libattr-devel ncurses-devel bc
