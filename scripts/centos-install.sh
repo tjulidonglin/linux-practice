5296c304bfb30a5180a75c300f7aa29ef710ee4f
lidonglin <tjulidonglin@live.cn>
2020-02-03 09:01:23 +0800

    centos-script.sh: add some rpm packages
    centos/redhat系列系统上执行kernel模块开发需要安装的依赖包

--- scripts/centos-install.sh
+++ scripts/centos-install.sh
@@ -2,3 +2,7 @@
 yum install -y gcc git wget vim cscope ctags
 #yum install -y openssh-server gcc g++ make autopoint pkgconf zlib1g-dev libsdl2-dev libglib2.0-dev libpixman-1-dev texinfo libusb-1.0-0-dev libspice-server-dev libncurses5-dev libtool autoconf libxml2-utils xsltproc libxml2-dev libpciaccess-dev libyajl-dev tree python3-pip libnl-3-dev libnl-route-3-dev libdevmapper-dev openvswitch-switch net-tools git vim samba openssh-server pkg-config gnutls-bin openvswitch-switch 
 yum install -y zlib-devel SDL-devel glib2-devel pixman-devel spice-server spice-server-devel libcap-devel libattr-devel ncurses-devel bc
+
+#centos/redhat系统支持kernel开发的依赖包
+yum install kernel kernel-headers kernel-devel kernel-tools kernel-tools-libs -y
+
