af5862db3176670ae4d9be34e83757ac867f905a
lidonglin <tjulidonglin@live.cn>
2020-02-09 16:31:42 +0800

    scripts: update deb list depended by qemu debug

--- scripts/qemu-config.sh
+++ scripts/qemu-config.sh
@@ -4,5 +4,6 @@ apt update
 echo "Begin to install dependent packages of qemu"
 apt install -y pkgconf zlib1g-dev libsdl2-dev libglib2.0-dev libpixman-1-dev texinfo libusb-1.0-0-dev g++ libspice-server-dev
 apt install -y libncurses5-dev #for seabios 
+apt install -y gdb #debug qemu
 echo "Finish installing dependent packages"
 
