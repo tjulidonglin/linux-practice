f4102d2ce37592fd2c8297bd1d5467118cab7f8a
lidonglin <tjulidonglin@live.cn>
2020-02-11 19:48:08 +0800

    libvirt-config.sh: libvirt编译依赖安装脚本重复了，删掉这个无用脚本

--- scripts/libvirt-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-apt update
-echo "Begin to install dependent packages of libvirt"
-apt install -y libtool autoconf autopoint libxml2-utils xsltproc libxml2-dev libpciaccess-dev libyajl-dev autopoint
-echo "Finish installing dependent packages"
-
