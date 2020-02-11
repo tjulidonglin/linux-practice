85f44719f606b59d44cb0c301c9aee4bcfc27341
lidonglin <tjulidonglin@live.cn>
2020-02-11 19:46:31 +0800

    linux-bridge.sh: 配置br0时添加依赖命令判断
    配置br0时检查依赖的命令是否存在，不存在先安装依赖的deb包再执行配置命令

--- scripts/linux-bridge.sh
+++ scripts/linux-bridge.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-brctl addbr br0
-brctl addif br0 eth0
-brctl stp br0 on
-ifconfig eth0 0
-dhclient br0
-echo "Success"
+if [ -f /usr/sbin/brctl ]
+then
+    brctl addbr br0
+    brctl addif br0 eth0
+    brctl stp br0 on
+    ifconfig eth0 0
+    dhclient br0
+    echo "Success"
+else
+	echo "Install bridge-utils first."
+	apt install -y bridge-utils
+    brctl addbr br0
+    brctl addif br0 eth0
+    brctl stp br0 on
+    ifconfig eth0 0
+    dhclient br0
+    echo "Success"
+fi
+
