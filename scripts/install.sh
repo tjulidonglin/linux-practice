ac4ca33a31d7b0e005c838ed991aac6edb10b8f4
lidonglin <tjulidonglin@live.cn>
2020-02-11 19:40:05 +0800

    install.sh: 将install.sh 脚本按照用途划分deb包
    install.sh 安装qemu编译依赖包、libvirt编译依赖包以及系统常用软件包

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,16 @@
-apt install gcc g++ make autopoint pkgconf zlib1g-dev libsdl2-dev libglib2.0-dev libpixman-1-dev texinfo libusb-1.0-0-dev libspice-server-dev libncurses5-dev libtool autoconf libxml2-utils xsltproc libxml2-dev libpciaccess-dev libyajl-dev tree python3-pip libnl-3-dev libnl-route-3-dev libdevmapper-dev openvswitch-switch net-tools git vim samba openssh-server pkg-config gnutls-bin openvswitch-switch -y
+#!/bin/bash
+
+# compile qemu
+apt install -y gcc g++ make autopoint pkgconf zlib1g-dev libsdl2-dev libglib2.0-dev libpixman-1-dev texinfo libusb-1.0-0-dev libspice-server-dev
+
+# compile libvirt
+apt install -y libncurses5-dev libtool autoconf libxml2-utils xsltproc libxml2-dev libpciaccess-dev libyajl-dev python3-pip libnl-3-dev libnl-route-3-dev libdevmapper-dev
+
+# install ovs
+apt install -y openvswitch-switch
+
+# system utils
+apt install -y git vim samba openssh-server pkg-config gnutls-bin tree
+
+# ifconfig
+apt install -y net-tools
