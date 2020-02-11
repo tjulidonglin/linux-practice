6648284287a84156654b8e0b0a7b1fbae874768c
lidonglin <tjulidonglin@live.cn>
2020-02-11 20:03:01 +0800

    shadowsocks: 将shadowsocks相关的脚本和配置文件归档到独立目录

---
+++ scripts/shadowsocks/shadowsocks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+apt update
+apt install -y python-pip
+pip install shadowsocks
+
+cat > /etc/shadowsocks.json <<EOF
+{
+	"server":"0.0.0.0",
+	"server_port":8976,
+	"local_port":1080,
+	"password":"Huawei123",
+	"timeout":600,
+	"method":"aes-256-cfb"
+}
+EOF
+ssserver -c /etc/shadowsocks.json -d start&
