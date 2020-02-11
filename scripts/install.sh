0e64b6f43fb43439a8be395c2a9a570029b76a9e
lidonglin <tjulidonglin@live.cn>
2020-02-11 19:53:51 +0800

    install.sh: 更新qemu编译依赖包的安装逻辑

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # compile qemu
-apt install -y gcc g++ make autopoint pkgconf zlib1g-dev libsdl2-dev libglib2.0-dev libpixman-1-dev texinfo libusb-1.0-0-dev libspice-server-dev
+apt install -y gdb gcc g++ make autopoint pkgconf zlib1g-dev libsdl2-dev libglib2.0-dev libpixman-1-dev texinfo libusb-1.0-0-dev libspice-server-dev
+
+# compile seabios
+apt install -y libncurses5-dev
 
 # compile libvirt
-apt install -y libncurses5-dev libtool autoconf libxml2-utils xsltproc libxml2-dev libpciaccess-dev libyajl-dev python3-pip libnl-3-dev libnl-route-3-dev libdevmapper-dev
+apt install -y libtool autoconf libxml2-utils xsltproc libxml2-dev libpciaccess-dev libyajl-dev python3-pip libnl-3-dev libnl-route-3-dev libdevmapper-dev
 
 # compile linux
 apt install -y libssl-dev dot2tex dvipng librsvg2-bin python3-sphinx libncurses5-dev
