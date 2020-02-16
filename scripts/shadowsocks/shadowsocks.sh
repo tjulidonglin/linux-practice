0a0f6ba4d93945812858b5c64ef30b16145a7610
lidonglin <tjulidonglin@live.cn>
2020-02-16 16:49:11 +0800

    shadowsocks: use new port number

--- scripts/shadowsocks/shadowsocks.sh
+++ scripts/shadowsocks/shadowsocks.sh
@@ -6,7 +6,7 @@ pip install shadowsocks
 cat > /etc/shadowsocks.json <<EOF
 {
 	"server":"0.0.0.0",
-	"server_port":8976,
+	"server_port":443,
 	"local_port":1080,
 	"password":"Huawei123",
 	"timeout":600,
