e7e52912ea6e24e664c65cc90bff177a1c82e28b
lidonglin <tjulidonglin@live.cn>
2020-02-22 16:53:00 +0800

    clean-ubuntu.sh: 添加需要删除的ubuntu软件列表

--- scripts/clean-ubuntu.sh
+++ scripts/clean-ubuntu.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-apt-get remove libreoffice-common
-apt-get remove unity-webapps-common
-apt-get remove thunderbird totem rhythmbox empathy brasero simple-scan gnome-mahjongg aisleriot gnome-mines cheese transmission-common gnome-orca webbrowser-app gnome-sudoku  landscape-client-ui-install
-apt-get remove onboard deja-dup
-apt-get remove gnome-software gnome-todo gedit gnome-power-manager update-manager update-manager-common update-nitifier update-notifier-common 
-apt-get autoremove
+apt remove -y libreoffice-common
+apt remove -y unity-webapps-common
+apt remove -y thunderbird totem rhythmbox empathy brasero simple-scan gnome-mahjongg aisleriot gnome-mines cheese transmission-common gnome-orca webbrowser-app gnome-sudoku  landscape-client-ui-install
+apt remove -y onboard deja-dup
+apt remove -y gnome-software gnome-todo gedit gnome-power-manager update-manager update-manager-common update-nitifier update-notifier-common 
+apt remove -y gnome-software
+apt remove -y update-manager
+apt remove -y software-properties-gtk
+apt remove -y gedit
+apt remove -y firefox
+apt autoremove -y
