7a321e7926751a1e265e3737c3891a4b3ef21a32
lidonglin <tjulidonglin@live.cn>
2020-02-23 18:01:12 +0800

    clean-ubuntu: change apt to apt-get
    旧版本的ubuntu系统不支持apt命令，卸载软件还是使用apt-get，为了兼容性还是采用apt-get代替apt

--- scripts/clean-ubuntu.sh
+++ scripts/clean-ubuntu.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-apt remove -y libreoffice-common
-apt remove -y unity-webapps-common
-apt remove -y thunderbird totem rhythmbox empathy brasero simple-scan gnome-mahjongg aisleriot gnome-mines cheese transmission-common gnome-orca webbrowser-app gnome-sudoku  landscape-client-ui-install
-apt remove -y onboard deja-dup
-apt remove -y gnome-software gnome-todo gedit gnome-power-manager update-manager update-manager-common update-nitifier update-notifier-common 
-apt remove -y gnome-software
-apt remove -y update-manager
-apt remove -y software-properties-gtk
-apt remove -y gedit
-apt remove -y firefox
-apt autoremove -y
+apt-get remove -y libreoffice-common
+apt-get remove -y unity-webapps-common
+apt-get remove -y thunderbird totem rhythmbox empathy brasero simple-scan gnome-mahjongg aisleriot gnome-mines cheese transmission-common gnome-orca webbrowser-app gnome-sudoku  landscape-client-ui-install
+apt-get remove -y onboard deja-dup
+apt-get remove -y gnome-software gnome-todo gedit gnome-power-manager update-manager update-manager-common update-nitifier update-notifier-common 
+apt-get remove -y gnome-software software-center
+apt-get remove -y update-manager-core
+apt-get remove -y ubuntuone-client
+apt-get remove -y update-manager
+apt-get remove -y software-properties-gtk
+apt-get remove -y gedit
+apt-get remove -y firefox
+apt-get autoremove -y
